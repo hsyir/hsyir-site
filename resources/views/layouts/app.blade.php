5fa935a159a13e37ce6b19309e6a2aaf34ab9402
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2020-12-19 11:40:58 +0330

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,15 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-V8R31S3QGE"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-V8R31S3QGE');
+    </script>
 
     <!-- Styles -->
     <link href="{{ asset('front/css/app.css') }}" rel="stylesheet">
